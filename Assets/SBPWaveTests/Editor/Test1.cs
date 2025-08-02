f42341dc46d903c33b80d63ca3bfc935b495afe8
eral <eral.r.l+github.com@gmail.com>
2025-08-02 16:21:36 +0900

    :rotating_light:Pass variant loading test for scriptableObject
    ScriptableObject用バリアント読み込みテストの通過

--- Assets/SBPWaveTests/Editor/Test1.cs
+++ Assets/SBPWaveTests/Editor/Test1.cs
@@ -11,25 +11,30 @@ public class Test1
 {
 	[UnityTest]
 	public IEnumerator LoadNormal() {
-		yield return LoadAndTest(kAssetBundleVariants[0], asset=>{
-			Assert.AreEqual("Test1 Int10001", asset.name);
+		yield return LoadAndTest(kAssetBundleVariants[0], (asset, ab)=>{
+			Assert.True(asset != null);
+			Assert.AreEqual(kAssetNames[1], asset.name);
 			Assert.AreEqual(10001, asset.Value);
+			Assert.AreEqual($"{kAssetBundleNames[1]}.{kAssetBundleVariants[0]}", ab.name);
 		});
 	}
 
 	[UnityTest]
 	public IEnumerator LoadVariant() {
-		yield return LoadAndTest(kAssetBundleVariants[1], asset=>{
-			Assert.AreEqual("Test1 Int10002", asset.name);
+		yield return LoadAndTest(kAssetBundleVariants[1], (asset, ab)=>{
+			Assert.True(asset != null);
+			Assert.AreEqual(kAssetNames[1], asset.name);
 			Assert.AreEqual(10002, asset.Value);
+			Assert.AreEqual($"{kAssetBundleNames[1]}.{kAssetBundleVariants[1]}", ab.name);
 		});
 	}
 
 	[UnityTest]
 	public IEnumerator NoLoad() {
-		yield return LoadAndTest(null, asset=>{
+		yield return LoadAndTest(null, (asset, ab)=>{
 			Assert.True(asset == null);
 			Assert.AreNotEqual(null, asset);
+			Assert.AreEqual(null, ab);
 		});
 	}
 
@@ -43,7 +48,7 @@ public class Test1
 					assetBundleName=kAssetBundleNames[0],
 					assetBundleVariant=null,
 					assetNames=new[]{
-						"Assets/SBPWaveTests/ScriptableObject/Test1Top.asset"
+						$"Assets/SBPWaveTests/ScriptableObject/{kAssetNames[0]}.asset"
 					},
 					addressableNames=null
 				},
@@ -51,7 +56,7 @@ public class Test1
 					assetBundleName=kAssetBundleNames[1],
 					assetBundleVariant=kAssetBundleVariants[0],
 					assetNames=new[]{
-						"Assets/SBPWaveTests/ScriptableObject/Test1 Int10001.asset"
+						$"Assets/SBPWaveTests/ScriptableObject/{kAssetBundleVariants[0]}/{kAssetNames[1]}.asset"
 					},
 					addressableNames=null
 				},
@@ -59,21 +64,22 @@ public class Test1
 					assetBundleName=kAssetBundleNames[1],
 					assetBundleVariant=kAssetBundleVariants[1],
 					assetNames=new[]{
-						"Assets/SBPWaveTests/ScriptableObject/Test1 Int10002.asset"
+						$"Assets/SBPWaveTests/ScriptableObject/{kAssetBundleVariants[1]}/{kAssetNames[1]}.asset"
 					},
 					addressableNames=null
 				},
 			},
-			options=BuildAssetBundleOptions.AssetBundleStripUnityVersion | BuildAssetBundleOptions.StripUnatlasedSpriteCopies,
+			options=BuildAssetBundleOptions.AssetBundleStripUnityVersion | BuildAssetBundleOptions.StripUnatlasedSpriteCopies | BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.DisableLoadAssetByFileNameWithExtension,
 			//targetPlatform=BuildTarget.StandaloneWindows,
 		});
 	}
 
 	private const string kAssetBundlesPath = "Assets/SBPWaveTests/AssetBundles~/Test1";
+	private readonly string[] kAssetNames = new[]{"Test1Top", "Test1Value" };
 	private readonly string[] kAssetBundleNames = new[]{"assetbundle", "assetbundle2" };
 	private readonly string[] kAssetBundleVariants = new[]{"int10001", "int10002" };
 
-	private IEnumerator LoadAndTest(string variant, Action<Test1IntValue> test) {
+	private IEnumerator LoadAndTest(string variant, Action<Test1IntValue, AssetBundle> test) {
 		var abcReq = AssetBundle.LoadFromFileAsync($"{kAssetBundlesPath}/{kAssetBundleNames[0]}");
 		AssetBundleCreateRequest abcReq2 = null;
 		if (variant != null) {
@@ -83,12 +89,12 @@ public class Test1
 		yield return abcReq;
 		var ab2 = abcReq2?.assetBundle;
 		var ab = abcReq.assetBundle;
-		var abReq = ab.LoadAssetAsync<Test1Top>("Test1Top");
+		var abReq = ab.LoadAssetAsync<Test1Top>(kAssetNames[0]);
 		yield return abReq;
 		var asset = (Test1Top)abReq.asset;
 		{
-			Assert.AreEqual("Test1Top", asset.name);
-			test(asset.Value);
+			Assert.AreEqual(kAssetNames[0], asset.name);
+			test(asset.Value, ab2);
 		}
 		ab.Unload(true);
 		ab2?.Unload(true);
