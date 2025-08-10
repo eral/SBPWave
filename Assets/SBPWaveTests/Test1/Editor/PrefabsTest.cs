8cfa3cfe66b973ceb7d871f77245c02f0c1bd443
eral <eral.r.l+github.com@gmail.com>
2025-08-11 05:50:57 +0900

    :rotating_light:Add variant loading test for prefab
    Prefab用バリアント読み込みテストの追加

---
+++ Assets/SBPWaveTests/Test1/Editor/PrefabsTest.cs
@@ -0,0 +1,180 @@
+using eral.SBPWave.Test.Internal.Editor;
+using NUnit.Framework;
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace eral.SBPWave.Test.Test1 {
+
+	public class PrefabsTest {
+		[UnityTest]
+		public IEnumerator LoadNormal() {
+			yield return TestNormal(TestUtility.Style.SBPWave);
+		}
+
+		[UnityTest]
+		public IEnumerator LoadVariant() {
+			yield return TestVariant(TestUtility.Style.SBPWave);
+		}
+
+		[UnityTest]
+		public IEnumerator NoLoad() {
+			yield return TestNoLoad(TestUtility.Style.SBPWave);
+		}
+
+		[UnityTest]
+		public IEnumerator LoadAssetDirectFromVariant() {
+			yield return TestLoadAssetDirectFromVariant(TestUtility.Style.SBPWave);
+		}
+
+		[UnityTest]
+		public IEnumerator LoadNormalBuiltin() {
+			yield return TestNormal(TestUtility.Style.Builtin);
+		}
+
+		[UnityTest]
+		public IEnumerator LoadVariantBuiltin() {
+			yield return TestVariant(TestUtility.Style.Builtin);
+		}
+
+		[UnityTest]
+		public IEnumerator NoLoadBuiltin() {
+			yield return TestNoLoad(TestUtility.Style.Builtin);
+		}
+
+		[UnityTest]
+		public IEnumerator LoadAssetDirectFromVariantBuiltin() {
+			yield return TestLoadAssetDirectFromVariant(TestUtility.Style.Builtin);
+		}
+
+		[OneTimeSetUp]
+		public void OneTimeSetUp() {
+			TestUtility.CallAllStyles(CreateAssetBundles);
+		}
+
+		private const string kAssetsBasePath = "Assets/SBPWaveTests/Test1/Runtime/Prefabs";
+		private const string kAssetBundlesPath = "Assets/SBPWaveTests/AssetBundles~/Prefabs_";
+		private readonly string[] kAssetNames = new[]{"Top", "Value"};
+		private readonly string[] kAssetBundleNames = new[]{"prefabs_top", "prefabs_value"};
+		private readonly string[] kAssetBundleVariants = new[]{"int10001", "int10002"};
+		private readonly int[] kAssetBundleVariantValues = new[]{10001, 10002};
+
+		private void CreateAssetBundles(TestUtility.Style style) {
+			var assetBundlesPath = TestUtility.AddStyleStringToEnd(style, kAssetBundlesPath);
+			TestUtility.CreateFolder(assetBundlesPath);
+			var builds = new[]{
+				new AssetBundleBuild{
+					assetBundleName=kAssetBundleNames[0],
+					assetBundleVariant=null,
+					assetNames=new[]{
+						$"{kAssetsBasePath}/{kAssetNames[0]}.prefab"
+					},
+					addressableNames=null
+				},
+				new AssetBundleBuild{
+					assetBundleName=kAssetBundleNames[1],
+					assetBundleVariant=kAssetBundleVariants[0],
+					assetNames=new[]{
+						$"{kAssetsBasePath}/{kAssetBundleVariants[0]}/{kAssetNames[1]}.prefab"
+					},
+					addressableNames=null
+				},
+				new AssetBundleBuild{
+					assetBundleName=kAssetBundleNames[1],
+					assetBundleVariant=kAssetBundleVariants[1],
+					assetNames=new[]{
+						$"{kAssetsBasePath}/{kAssetBundleVariants[1]}/{kAssetNames[1]}.prefab"
+					},
+					addressableNames=null
+				},
+			};
+			var assetBundleOptions = BuildAssetBundleOptions.AssetBundleStripUnityVersion | BuildAssetBundleOptions.ForceRebuildAssetBundle | BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.DisableLoadAssetByFileNameWithExtension
+#if UNITY_2022_1_OR_NEWER
+			                       | BuildAssetBundleOptions.StripUnatlasedSpriteCopies
+#endif
+			;
+			var targetPlatform = EditorUserBuildSettings.activeBuildTarget;
+			TestUtility.BuildAssetBundles(style, assetBundlesPath, builds, assetBundleOptions, targetPlatform);
+		}
+
+		private IEnumerator TestNormal(TestUtility.Style style) {
+			yield return LoadAndTest(style, kAssetBundleVariants[0], (script, ab)=>{
+				Assert.True(script != null);
+				Assert.AreEqual(kAssetBundleVariantValues[0], script.Value);
+				Assert.AreEqual($"{kAssetBundleNames[1]}.{kAssetBundleVariants[0]}", ab.name);
+			});
+		}
+
+		private IEnumerator TestVariant(TestUtility.Style style) {
+			yield return LoadAndTest(style, kAssetBundleVariants[1], (script, ab)=>{
+				Assert.True(script != null);
+				Assert.AreEqual(kAssetBundleVariantValues[1], script.Value);
+				Assert.AreEqual($"{kAssetBundleNames[1]}.{kAssetBundleVariants[1]}", ab.name);
+			});
+		}
+
+		private IEnumerator TestNoLoad(TestUtility.Style style) {
+			yield return LoadAndTest(style, null, (script, ab)=>{
+				Assert.True(script == null);
+				Assert.AreEqual(null, script);
+				Assert.AreEqual(null, ab);
+			});
+		}
+
+		private IEnumerator TestLoadAssetDirectFromVariant(TestUtility.Style style) {
+			var assetBundlesPath = TestUtility.AddStyleStringToEnd(style, kAssetBundlesPath);
+			for (var i = 0; i < kAssetBundleVariants.Length; i++) {
+				var abcReq = AssetBundle.LoadFromFileAsync($"{assetBundlesPath}/{kAssetBundleNames[1]}.{kAssetBundleVariants[i]}");
+				while (!abcReq.isDone) yield return null;
+				var ab = abcReq.assetBundle;
+				var abReq = ab.LoadAssetAsync<GameObject>(kAssetNames[1]);
+				while (!abReq.isDone) yield return null;
+				var prefab = (GameObject)abReq.asset;
+				{
+					Assert.AreEqual(kAssetNames[1], prefab.name);
+					var govalue = Object.Instantiate(prefab);
+					var value = govalue.GetComponentInChildren<PrefabsValue>();
+					Assert.AreEqual(kAssetBundleVariantValues[i], value.Value);
+					Object.DestroyImmediate(govalue);
+				}
+				ab.Unload(true);
+			}
+		}
+
+		private IEnumerator LoadAndTest(TestUtility.Style style, string variant, System.Action<PrefabsValue, AssetBundle> test) {
+			var assetBundlesPath = TestUtility.AddStyleStringToEnd(style, kAssetBundlesPath);
+			var abcReq = AssetBundle.LoadFromFileAsync($"{assetBundlesPath}/{kAssetBundleNames[0]}");
+			AssetBundleCreateRequest abcReq2 = null;
+			if (variant != null) {
+				abcReq2 = AssetBundle.LoadFromFileAsync($"{assetBundlesPath}/{kAssetBundleNames[1]}.{variant}");
+				while (!abcReq2.isDone) yield return null;
+			}
+			while (!abcReq.isDone) yield return null;
+			var ab2 = abcReq2?.assetBundle;
+			var ab = abcReq.assetBundle;
+			var abReq = ab.LoadAssetAsync<GameObject>(kAssetNames[0]);
+			while (!abReq.isDone) yield return null;
+			var prefab = (GameObject)abReq.asset;
+			{
+				Assert.AreEqual(kAssetNames[0], prefab.name);
+				var gotop = Object.Instantiate(prefab);
+				var top = gotop.GetComponentInChildren<PrefabsTop>();
+				var govalue = default(GameObject);
+				var value = default(PrefabsValue);
+				if (top.Value != null) {
+					govalue = Object.Instantiate(top.Value);
+					value = govalue.GetComponentInChildren<PrefabsValue>();
+				}
+				test(value, ab2);
+				if (govalue != null) {
+					Object.DestroyImmediate(govalue);
+				}
+				Object.DestroyImmediate(gotop);
+			}
+			ab.Unload(true);
+			ab2?.Unload(true);
+		}
+	}
+
+}
